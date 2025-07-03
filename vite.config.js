40acb0b4eb0a2635726faf0826e1aef553c78b5d
User <dolgikh.020@mail.ru>
2025-07-03 11:54:46 -0500

    first commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "./",
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
+})
